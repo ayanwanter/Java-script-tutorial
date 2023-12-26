4570d5b20fff58d205d46597d92d4966ca087ee3
ayanwanter <132154828+ayanwanter@users.noreply.github.com>
2023-12-26 12:33:45 +0000

    Datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,29 @@
+"use strict"  // treat all js version as new version
+
+// alert( 3+3 ) // we are using node js , not browser
+
+
+console.log(3
+    +3) // code readability should be high
+
+console.log("Ayan")
+
+
+let name = "Ayan"
+let age = 18
+let inLoggedIn = false
+
+// number  =>  2 to power 53
+// bigint
+// string =>
+//boolean => true/false
+// null => standalone value
+//undefined =>
+//symbol => unique
+
+
+
+//object
+
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
